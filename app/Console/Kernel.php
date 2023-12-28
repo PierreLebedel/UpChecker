9af5fff5d398c0db4db36252281bd000ae1eb79b
PierreLebedel <pierrelebedel@gmail.com>
2023-12-28 18:59:41 +0100

    kernel schedule endpoint loop

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\EndpointCheckJob;
+use App\Models\Endpoint;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +15,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        $endpoints = Endpoint::all();
+
+        foreach($endpoints as $endpoint){
+            //$schedule->job( new EndpointCheckJob($endpoint) )->everyMinute();
+        }
     }
 
     /**
